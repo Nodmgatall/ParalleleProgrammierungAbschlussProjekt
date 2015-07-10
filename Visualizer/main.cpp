0268849655e4b736de1f7ce9b6898785f231c95b
NodmGatall <oliverheidmann@hotmail.de>
2015-07-10 13:03:48 +0200

    redoing commits

--- Visualizer/main.cpp
+++ Visualizer/main.cpp
@@ -3,5 +3,7 @@
 int main(int argc, char** argv)
 {
     Visualizer visualizer;
+
+    return EXIT_SUCCESS;
 }
 
--- Visualizer/visualizer.hpp
+++ Visualizer/visualizer.hpp
@@ -13,15 +13,18 @@ class Visualizer
         int m_screen_width;
         int m_screen_height;
         bool m_running;
+        bool m_pause;
+        unsigned long m_iteration_number;
         ResourceManager m_resource_manager;
         SDL_Renderer *m_renderer;
         SDL_Window *m_window;
         
         void init_SDL();
-        void load_object_data_from_file();
+        void load_object_data_from_file(std::string filepath);
         void load_textures();
         void render_texture(SDL_Texture *tex, int x, int y, int z);
-        void main_loop();
+        void main_loop(double dt);
+        void pause_loop();
         void update();
     
     public:
@@ -30,7 +33,7 @@ class Visualizer
         Visualizer(int screen_height, int sreen_width);
         ~Visualizer();
         
-        void draw(unsigned long interation_number, double dt);
+        void draw_main_loop(unsigned long iteration_number, double dt);
 };
 
 #endif
