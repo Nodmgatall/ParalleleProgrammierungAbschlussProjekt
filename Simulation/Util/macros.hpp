5ed3983a3550d4c7b7fb214f2b6096c7a5e8ec8d
tronje <tronjekrabbe@gmail.com>
2015-09-07 17:39:57 +0200

    macros: tweak macros.hpp to accomodate parallelization, i.e. the DEBUG macro is only run by rank 0, if at all [parallel]

--- Simulation/Util/macros.hpp
+++ Simulation/Util/macros.hpp
@@ -1,8 +1,9 @@
 #ifndef MACROS_HPP
 #define MACROS_HPP
+#include "../globals.h"
 
 #ifdef DEBUG_BUILD
-    #define DEBUG(X) std::cerr << X << std::endl
+    #define DEBUG(X) if(rank == 0) std::cerr << X << std::endl
 #else
     #define DEBUG(X)
 #endif
